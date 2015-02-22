b9fe7c56ff4f48e0bb292725db9a332323510b80
lidenscheng <lidenscheng@email.arizona.edu>
2015-02-22 15:01:19 -0700

    modified to geom_bar

--- gglot_examples.r
+++ gglot_examples.r
@@ -12,9 +12,29 @@ library("ggthemes")
 library("reshape2")
 
 myplot <- ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
-  geom_point(size=3)
+  geom_point(size=3) #separae species by different colors
 myplot
 
 ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
-  geom_point(aes(shape= Species), size = 3)
-#summary(myplot)
+  geom_point(aes(shape= Species), size = 3) #separate species by different shapes also
+
+d2 <- diamonds[sample(1:dim(diamonds)[1], 1000), ] #samples a random group from diamonds
+ggplot(d2, aes(x= carat, y = price, color = color))+ 
+  geom_point()
+
+library(MASS)
+myplot <- ggplot(birthwt, aes(factor(race), bwt)) + geom_boxplot()
+summary(myplot)
+
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
+  geom_point()+ facet_grid(. ~ Species)
+#facet grid 
+#facet wrap 
+
+library(RColorBrewer)
+display.brewer.all()
+
+df <- melt(iris, id.vars = "Species")
+ggplot(df, aes(Species, value, fill=variable))+
+  geom_bar(stat= "identity", position="dodge")+
+  scale_fill_brewer(palette ="Set1")
