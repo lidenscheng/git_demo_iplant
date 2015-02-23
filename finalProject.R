18a49cd33db478a0a3336d6aab808c299fe3b7d8
Lidens Cheng <lidenscheng@vm65-219.iplantcollaborative.org>
2015-02-22 17:03:59 -0700

    final project code

---
+++ finalProject.R
@@ -0,0 +1,13 @@
+#Lidens Cheng
+#Feb 21, 2015
+#running on Atm
+
+install.packages("ggplot2", dependencies = TRUE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2")
+
+library("ggplot2")
+library("reshape2")
+library("ggthemes")
+library("reshape2")
